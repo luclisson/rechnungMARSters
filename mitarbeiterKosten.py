d22265c9e5b15925378d4ec29d1cab8dc6aed5a0
Luc <lisson.luc@gmail.com>
2025-07-15 22:33:06 +0200

    restructuring done

--- mitarbeiterKosten.py
+++
@@ -1,9 +0,0 @@
-class MitarbeiterKosten:
-    def __init__(self, leistung, stunden, satz, gesamtkosten):
-        self.leistung = leistung
-        self.stunden = stunden
-        self.satz = satz
-        self.gesamtkosten = gesamtkosten
-    
-    def __str__(self):
-        return f"Leistung: {self.leistung}, Stunden: {self.stunden}, Satz: {self.satz}, Gesamtkosten: {self.gesamtkosten}"
--- usedGood.py
+++
@@ -1,8 +0,0 @@
-class UsedGood:
-    def __init__(self, leistung, mwst, einzelpreis, anzahl,gesamtpreis):
-        self.leistung = leistung
-        self.mwst = mwst
-        self.einzelpreis = einzelpreis
-        self.anzahl = anzahl
-        self.gesamtpreis = gesamtpreis
-    
